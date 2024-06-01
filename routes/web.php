696390810a9a7d3aaf1f64bb0e99a7517f339441
LailafinNahar-Tithy <nahartithty90@gmail.com>
2024-06-01 17:58:24 +0600

    3rd day

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,4 @@ Route::get('/about-us', [PublicController::class,'about'])->name('about');
 Route::get('/contact',[PublicController::class,'contact']);
 Route::get('/users',[PublicController::class,'users'])->name('users');
 Route::get('/dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
+Route::get('/products',[ProductController::class,'index'])->name('products.index');
